d81678ab9974b58a5b5a968a5d3bd01e79ee3777
Ahmed Faisal <ah.faisal@gmail.com>
2020-06-15 06:32:22 +0500

    Updates

---
+++ build/precache-manifest.e892ce3576ef624571fa3d6a171667ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81621e64e84a21f5e17598997371dac9",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "3222eed2bffc777f8105",
+    "url": "/react-tutorial/static/css/main.cb3f2ade.chunk.css"
+  },
+  {
+    "revision": "4cfc1a60f95ad5334208",
+    "url": "/react-tutorial/static/js/2.0da6d042.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0da6d042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3222eed2bffc777f8105",
+    "url": "/react-tutorial/static/js/main.f53bfe51.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
